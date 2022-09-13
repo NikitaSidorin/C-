c3d86cde97e882305c76f1ef706595c3b2e2d84d
nikita sidorin <nikittoss12@mail.ru>
2022-09-13 21:13:13 +0700

    10 задача

---
+++ Задача10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int amount = num.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+     Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{  
+     int secondnum = ((num / 10) % 10 );
+     Console.WriteLine(secondnum);
+}
+
