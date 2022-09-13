4f4add91eb2e0b4b3959fcee363b1c6ae4d08846
nikita sidorin <nikittoss12@mail.ru>
2022-09-13 21:44:26 +0700

    задача 15

---
+++ Задача15/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.Write("Введите номер дня (1-7): ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+ switch(num1)
+ {
+    case 1:
+    Console.WriteLine("Понедельник");
+    break;
+    
+    case 2:
+    Console.WriteLine("Вторник");
+    break;
+    
+    case 3:
+    Console.WriteLine("Среда");
+    break;
+    
+    case 4:
+    Console.WriteLine("Четверг");
+    break;
+    
+    case 5:
+    Console.WriteLine("Пятница");
+    break;
+    
+    case 6:
+    Console.WriteLine("Суббота");
+    break;
+     
+    case 7:
+    Console.WriteLine("Воскресенье");
+    break;
+
+    default:
+    Console.WriteLine("Число не входит в диапозон 1..7 ");
+    break;
+}
+switch(num1)
+ {
+    case 1:
+    Console.WriteLine("Рабочий день");
+    break;
+    
+    case 2:
+    Console.WriteLine("Рабочий день");
+    break;
+    
+    case 3:
+    Console.WriteLine("Рабочий день");
+    break;
+    
+    case 4:
+    Console.WriteLine("Рабочий день");
+    break;
+    
+    case 5:
+    Console.WriteLine("Рабочий день");
+    break;
+    
+    case 6:
+    Console.WriteLine("Выходной");
+    break;
+     
+    case 7:
+    Console.WriteLine("Выходной");
+    break;
+
+    default:
+    Console.WriteLine("Число не входит в диапозон 1..7 ");
+    break;
+ }
