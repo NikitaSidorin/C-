5727719a34e2cba6a33691704627b979ce7fbefd
nikita sidorin <nikittoss12@mail.ru>
2022-09-13 21:18:09 +0700

    задача 13

---
+++ Задача13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int amount = num.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+     Console.WriteLine("Нет третьей цифры");
+}
+else
+{  
+     int secondnum = (num  % 10 );
+     Console.WriteLine(secondnum);
+}
+
